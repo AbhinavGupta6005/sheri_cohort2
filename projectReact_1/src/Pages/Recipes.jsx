7b30ebd32a72f42559847f6495bdbd46386c8122
Abhinav Gupta <motilalg25@gmail.com>
2025-06-20 11:20:23 +0530

    failed to push on the recipes section

--- projectReact_1/src/Pages/Recipes.jsx
+++ projectReact_1/src/Pages/Recipes.jsx
@@ -1,15 +1,19 @@
 import { useContext } from "react";
-import {recipecontext } from "../context/RecipeContext";
-import { useState } from "react";
+import { recipecontext } from "../context/RecipeContext";
+import { useState, useEffect } from "react";
 
 const Recipes = () => {
   const { data } = useContext(recipecontext);
 
   const [recipe, setRecipe] = useState(data);
-  useEffect(()=> {
+  useEffect(() => {
     setRecipe(data)
-  },[data])
-  return <div>{recipe}</div>;
+  }, [data])
+
+
+  return <div>{recipe.map((recipe, i) => (
+    <h1 key={i.id}>{recipe.title}</h1>
+  ))}</div>;
 };
 
 export default Recipes;
